1fbc6637e55a7215f2992f68445a9195794d7cf9
Aneeqa Fatima <aneeqaf@umich.edu>
2017-12-10 17:13:54 -0500

    Inefficient CZ opt. Works for only 0 to 2 incident CZ gates

--- Quantum Circuit/circuit_simulation.h
+++ Quantum Circuit/circuit_simulation.h
@@ -38,15 +38,10 @@ using index_size = size_t;
 class state {
 public:
     vector<cmplx> amp;
-    vector<cmplx> apply_gate;
-    vector<index_size> indices_for_ag;
-    
-    void operator*=(const gate& matrix);
     
     cmplx measure_0(const short qubit);
     cmplx measure_1(const short qubit);
     void measure(const short qubit);
-    vector<cmplx> tensor();
     
     state();
     state(const state& rhs);
@@ -74,15 +69,15 @@ public:
     index_size global_factor_power;
     
     template<typename function>
-    vector<index_size> FormBlockOfGates(int& num_X_gates,
-                                        index_size gate_i,
-                                        vector<index_size>& T_bit_mask,
-                                        function comp);
-    void ApplyBlockOfGates(const vector<index_size>& cbits,
-                           index_size gate_i,
-                           const vector<index_size>& T,
-                           int X_gates);
-    inline void FindRelevantAmp(vector<bool>& iterated,
+    pair<index_size, index_size>* FormBlockOfGates(const vector<gate>& gates,
+                                                   index_size& gate_i,
+                                                    vector<index_size>& T_bit_mask,
+                                                    function comp);
+    void ApplyBlockOfGates(const pair<index_size, index_size>* cbits,
+                           const index_size cbits_size,
+                           vector<cmplx>& amp,
+                           const vector<index_size>& T);
+    inline void FindRelevantAmp(vector<index_size>& indices_amp_gate,
                                 index_size gate_i,
                                 const int num_bits,
                                 const index_size idx);
@@ -91,20 +86,24 @@ public:
     inline void ApplyNonControlGates(const index_size gate_i);
     inline void  ApplyCXGate(const index_size idx,
                              const index_size swap_index);
-    inline void ApplyPhaseGate(const int i_count,
-                               const index_size idx);
-    inline void ApplyTGateKTimes(const index_size gate_c,
-                                 const index_size idx);
+    inline cmplx ApplyPhaseGate(const index_size gate_c,
+                               const cmplx& amp);
+    inline cmplx ApplyTGateKTimes(const index_size gate_c,
+                                  const cmplx& amp);
     inline int ApplyHOnAllAmp(const index_size gate_i);
-    inline void ApplyXX12Gate();
-    inline void ApplyXY12Gate();
-    inline void ApplyYY12Gate();
-    inline void ApplyYX12Gate();
+    inline void ApplyXX12Gate(const index_size* const indices);
+    inline void ApplyXY12Gate(const index_size* const indices,
+                              const index_size size);
+    inline void ApplyYY12Gate(const index_size* constindices);
+    inline void ApplyYX12Gate(const index_size* const indices);
     void Merge2QXY12Gates(const index_size gate_i);
     void ApplyMergedXY12Gates(const short type,
                             const index_size gate_i);
     void ApplySingleTGate(const index_size gate_i);
     void ApplySingleCPhaseGate(const index_size gate_i);
+    void ApplyGateOnGateSizeAmps(const index_size* const indices,
+                                 const index_size size,
+                                 const gate& q_gate);
     void GroupAlternateCycles();
     void GroupSimilarGates();
     float CalculateNormOfAmp();
