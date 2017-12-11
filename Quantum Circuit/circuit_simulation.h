08fe9d8afcfe4a5d234cecadae5c0d4ec25f32da
Aneeqa Fatima <aneeqaf@umich.edu>
2017-12-10 20:37:30 -0500

    Implemented most of Prof. Markov's suggestions

--- Quantum Circuit/circuit_simulation.h
+++ Quantum Circuit/circuit_simulation.h
@@ -33,7 +33,7 @@ using namespace std;
 
 const float HADAMARD_CONST = 1/sqrt(2);
 
-using index_size = size_t;
+using idx_size = size_t;
 
 class state {
 public:
@@ -66,43 +66,39 @@ public:
     bool google ;
     //1ull < (global_factor_power/2) * [[global_factor_power%2 == 1]] * sqrt(2)
     //global_factor_power represents the count of H, X_1_2, and Y_1_2 gates.
-    index_size global_factor_power;
+    idx_size global_factor_power;
     
     template<typename function>
-    index_size* FormBlockOfGates(const vector<gate>& gates,
-                                index_size& gate_i,
-                                vector<index_size>& T_bit_mask,
+    idx_size* FormBlockOfGates(const vector<gate>& gates,
+                                idx_size& gate_i,
+                                vector<idx_size>& T_bit_mask,
                                 function comp);
-    void ApplyBlockOfGates(const index_size* cbits,
+    void ApplyBlockOfGates(const idx_size* cbits,
                            const int qubits,
                            vector<cmplx>& amp,
-                           const vector<index_size>& T);
-    inline void FindRelevantAmp(vector<index_size>& indices_amp_gate,
-                                index_size gate_i,
+                           const vector<idx_size>& T);
+    inline void FindRelevantAmp(vector<idx_size>& indices_amp_gate,
+                                idx_size gate_i,
                                 const int num_bits,
-                                const index_size idx);
-    inline void ApplyAnyCGate(const index_size c_bits,
-                              const index_size gate_i);
-    inline void ApplyNonControlGates(const index_size gate_i);
-    inline void  ApplyCXGate(const index_size idx,
-                             const index_size swap_index);
-    inline cmplx ApplyPhaseGate(const index_size gate_c,
-                               const cmplx& amp);
-    inline cmplx ApplyTGateKTimes(const index_size gate_c,
-                                  const cmplx& amp);
-    inline int ApplyHOnAllAmp(const index_size gate_i);
-    inline void ApplyXX12Gate(const index_size* const indices);
-    inline void ApplyXY12Gate(const index_size* const indices,
-                              const index_size size);
-    inline void ApplyYY12Gate(const index_size* constindices);
-    inline void ApplyYX12Gate(const index_size* const indices);
-    void Merge2QXY12Gates(const index_size gate_i);
+                                const idx_size idx);
+    inline void ApplyAnyCGate(const idx_size c_bits,
+                              const idx_size gate_i);
+    inline void ApplyNonControlGates(const idx_size gate_i);
+    inline void  ApplyCXGate(const idx_size idx,
+                             const idx_size swap_index);
+    inline int ApplyHOnAllAmp(const idx_size gate_i);
+    inline void ApplyXX12Gate(const idx_size* const indices);
+    inline void ApplyXY12Gate(const idx_size* const indices,
+                              const idx_size size);
+    inline void ApplyYY12Gate(const idx_size* constindices);
+    inline void ApplyYX12Gate(const idx_size* const indices);
+    void Merge2QXY12Gates(const idx_size gate_i);
     void ApplyMergedXY12Gates(const short type,
-                            const index_size gate_i);
-    void ApplySingleTGate(const index_size gate_i);
-    void ApplySingleCPhaseGate(const index_size gate_i);
-    void ApplyGateOnGateSizeAmps(const index_size* const indices,
-                                 const index_size size,
+                            const idx_size gate_i);
+    void ApplySingleTGate(const idx_size gate_i);
+    void ApplySingleCPhaseGate(const idx_size gate_i);
+    void ApplyGateOnGateSizeAmps(const idx_size* const indices,
+                                 const idx_size size,
                                  const gate& q_gate);
     void GroupAlternateCycles();
     void GroupSimilarGates();
