34e8e7b6d66b96304abbdf257e9ee342439df394
Aneeqa Fatima <aneeqaf@umich.edu>
2018-01-03 02:22:40 +0300

    XMM impl : syntactic changes and no calls to real() and imag()

--- Quantum Circuit/sim/kernels/kernels1.h
+++ Quantum Circuit/sim/kernels/kernels1.h
@@ -17,86 +17,92 @@ __attribute__((always_inline)) inline void
 Apply4X12Gate(cmplx* __restrict amp,
               const array<idx_size, 16> indices)
 {
-     const cmplx a[16] = {amp[indices[0]], amp[indices[4]],  amp[indices[8]], amp[indices[12]],
-                        amp[indices[1]], amp[indices[5]],  amp[indices[9]], amp[indices[13]],
-                        amp[indices[2]], amp[indices[6]],  amp[indices[10]], amp[indices[14]],
-                        amp[indices[3]], amp[indices[7]], amp[indices[11]],  amp[indices[15]]};
+      cmplx a[16] __attribute__((aligned(64))) =
+                    {amp[indices[0]], amp[indices[4]],  amp[indices[8]], amp[indices[12]],
+                    amp[indices[1]], amp[indices[5]],  amp[indices[9]], amp[indices[13]],
+                    amp[indices[2]], amp[indices[6]],  amp[indices[10]], amp[indices[14]],
+                    amp[indices[3]], amp[indices[7]], amp[indices[11]],  amp[indices[15]]};
 
-    __m128 t_real = _mm_setr_ps(real(a[0]), real(a[4]), real(a[8]), real(a[12]));
-    __m128 t1_real = _mm_setr_ps(real(a[1]), real(a[5]), real(a[9]), real(a[13]));
-    __m128 t2_real = _mm_setr_ps(real(a[2]), real(a[6]), real(a[10]), real(a[14]));
-    __m128 t3_real = _mm_setr_ps(real(a[3]), real(a[7]), real(a[11]), real(a[15]));
+    float* __restrict b_real = (float*)(&a[0]);
+    float* __restrict b_imag = (float*)(&a[0]) + 1;
     
-    __m128 t_imag = _mm_setr_ps(imag(a[0]), imag(a[4]), imag(a[8]), imag(a[12]));
-    __m128 t1_imag = _mm_setr_ps(imag(a[1]), imag(a[5]), imag(a[9]), imag(a[13]));
-    __m128 t2_imag = _mm_setr_ps(imag(a[2]), imag(a[6]), imag(a[10]), imag(a[14]));
-    __m128 t3_imag = _mm_setr_ps(imag(a[3]), imag(a[7]), imag(a[11]), imag(a[15]));
-    
-    __m128 tres = _mm_add_ps(t_real, t3_real);
+    const __m128 t_real = _mm_setr_ps(*b_real, *(b_real + 8), *(b_real + 16), *(b_real + 24));
+    b_real+=2;
+    const __m128 t1_real = _mm_setr_ps(*b_real, *(b_real + 8), *(b_real + 16), *(b_real + 24));
+    b_real+=2;
+    const __m128 t2_real = _mm_setr_ps(*b_real, *(b_real + 8), *(b_real + 16), *(b_real + 24));
+    b_real+=2;
+    const __m128 t3_real = _mm_setr_ps(*b_real, *(b_real + 8), *(b_real + 16), *(b_real + 24));
+
+    const __m128 t_imag = _mm_setr_ps(*b_imag, *(b_imag + 8), *(b_imag + 16), *(b_imag + 24));
+    b_imag+=2;
+    const __m128 t1_imag = _mm_setr_ps(*b_imag, *(b_imag + 8), *(b_imag + 16), *(b_imag + 24));
+    b_imag+=2;
+    const __m128 t2_imag = _mm_setr_ps(*b_imag, *(b_imag + 8), *(b_imag + 16), *(b_imag + 24));
+    b_imag+=2;
+    const __m128 t3_imag = _mm_setr_ps(*b_imag, *(b_imag + 8), *(b_imag + 16), *(b_imag + 24));
+
+    __m128 tres0 = _mm_add_ps(t_real, t3_real);
     __m128 tres1 = _mm_add_ps(t1_real, t2_real);
     __m128 tres2 = _mm_sub_ps(t3_imag, t_imag);
     __m128 tres3 = _mm_sub_ps(t2_imag, t1_imag);
     
     __m128 res_real_add = _mm_add_ps(tres1, tres2);
-    __m128 res1_real_add = _mm_add_ps(tres, tres3);
-    __m128 res_real_sub = _mm_sub_ps(tres, tres3);
+    __m128 res1_real_add = _mm_add_ps(tres0, tres3);
+    __m128 res_real_sub = _mm_sub_ps(tres0, tres3);
     __m128 res1_real_sub = _mm_sub_ps(tres1, tres2);
     
-    tres = _mm_add_ps(t_imag, t3_imag);
+    tres0 = _mm_add_ps(t_imag, t3_imag);
     tres1 = _mm_add_ps(t1_imag, t2_imag);
     tres2 = _mm_sub_ps(t_real, t3_real);
     tres3 = _mm_sub_ps(t1_real, t2_real);
     
     __m128 res_imag_add = _mm_add_ps(tres1, tres2);
-    __m128 res1_imag_add = _mm_add_ps(tres, tres3);
-    __m128 res_imag_sub = _mm_sub_ps(tres, tres3);
+    __m128 res1_imag_add = _mm_add_ps(tres0, tres3);
+    __m128 res_imag_sub = _mm_sub_ps(tres0, tres3);
     __m128 res1_imag_sub = _mm_sub_ps(tres1, tres2);
     
-    //slice1
-    res_real_add = _mm_permute_ps(res_real_add, 0b10011100);
-    res_imag_add = _mm_permute_ps(res_imag_add, 0b01100011);
-    //slice2
-    res1_real_add = _mm_permute_ps(res1_real_add, 0b10011100);
-    res1_imag_add = _mm_permute_ps(res1_imag_add, 0b01100011);
-    //slice3
-    res_real_sub = _mm_permute_ps(res_real_sub, 0b10011100);
-    res_imag_sub = _mm_permute_ps(res_imag_sub, 0b01100011);
-    //slice4
-    res1_real_sub = _mm_permute_ps(res1_real_sub, 0b10011100);
-    res1_imag_sub = _mm_permute_ps(res1_imag_sub, 0b01100011);
+    const __m128 slice1_real = _mm_permute_ps(res_real_add, 0b10011100);
+    const __m128 slice1_imag = _mm_permute_ps(res_imag_add, 0b01100011);
+    const __m128 slice2_real = _mm_permute_ps(res1_real_add, 0b10011100);
+    const __m128 slice2_imag = _mm_permute_ps(res1_imag_add, 0b01100011);
+    const __m128 slice3_real = _mm_permute_ps(res_real_sub, 0b10011100);
+    const __m128 slice3_imag = _mm_permute_ps(res_imag_sub, 0b01100011);
+    const __m128 slice4_real = _mm_permute_ps(res1_real_sub, 0b10011100);
+    const __m128 slice4_imag = _mm_permute_ps(res1_imag_sub, 0b01100011);
     
-    tres = _mm_hadd_ps(res_real_add, res1_real_add);
-    tres1 = _mm_hadd_ps(res_real_sub, res1_real_sub);
-    tres2 = _mm_hsub_ps(res_imag_add, res1_imag_add);
-    tres3 = _mm_hsub_ps(res_imag_sub, res1_imag_sub);
+    tres0 = _mm_hadd_ps(slice1_real, slice2_real);
+    tres1 = _mm_hadd_ps(slice3_real, slice4_real);
+    tres2 = _mm_hsub_ps(slice1_imag, slice2_imag);
+    tres3 = _mm_hsub_ps(slice3_imag, slice4_imag);
     tres2 = _mm_permute_ps(tres2, 0b10110001);
     tres3 = _mm_permute_ps(tres3, 0b10110001);
     
-    __m128 tres4 = _mm_add_ps(tres, tres2);
-    __m128 tres5 = _mm_sub_ps(tres, tres2);
-    __m128 tres6 = _mm_add_ps(tres1, tres3);
-    __m128 tres7 = _mm_sub_ps(tres1, tres3);
+    res_real_add = _mm_add_ps(tres0, tres2);
+    res_real_sub = _mm_sub_ps(tres0, tres2);
+    res1_real_add = _mm_add_ps(tres1, tres3);
+    res1_real_sub = _mm_sub_ps(tres1, tres3);
     
-    tres = _mm_hadd_ps(res_imag_add, res1_imag_add);
-    tres1 = _mm_hadd_ps(res_imag_sub, res1_imag_sub);
-    tres2 = _mm_hsub_ps(res_real_add, res1_real_add);
-    tres3 = _mm_hsub_ps(res_real_sub, res1_real_sub);
+    tres0 = _mm_hadd_ps(slice1_imag, slice2_imag);
+    tres1 = _mm_hadd_ps(slice3_imag, slice4_imag);
+    tres2 = _mm_hsub_ps(slice1_real, slice2_real);
+    tres3 = _mm_hsub_ps(slice3_real, slice4_real);
     tres2 = _mm_permute_ps(tres2, 0b10110001);
     tres3 = _mm_permute_ps(tres3, 0b10110001);
     
-    res_imag_add = _mm_add_ps(tres, tres2);
-    res_imag_sub = _mm_sub_ps(tres, tres2);
+    res_imag_add = _mm_add_ps(tres0, tres2);
+    res_imag_sub = _mm_sub_ps(tres0, tres2);
     res1_imag_add = _mm_add_ps(tres1, tres3);
     res1_imag_sub = _mm_sub_ps(tres1, tres3);
 
-    float* res_real = (float*)&tres4;
-    float* res_imag = (float*)&res_imag_add;
-    float* res1_real = (float*)&tres5;
-    float* res1_imag = (float*)&res_imag_sub;
-    float* res2_real = (float*)&tres6;
-    float* res2_imag = (float*)&res1_imag_add;
-    float* res3_real = (float*)&tres7;
-    float* res3_imag = (float*)&res1_imag_sub;
+    const float* res_real = (float*)&res_real_add;
+    const float* res_imag = (float*)&res_imag_add;
+    const float* res1_real = (float*)&res_real_sub;
+    const float* res1_imag = (float*)&res_imag_sub;
+    const float* res2_real = (float*)&res1_real_add;
+    const float* res2_imag = (float*)&res1_imag_add;
+    const float* res3_real = (float*)&res1_real_sub;
+    const float* res3_imag = (float*)&res1_imag_sub;
     
     amp[indices[0]] = cmplx(res_real[1], res_imag[1]); amp[indices[1]] = cmplx(res_real[0], res_imag[0]);
     amp[indices[2]] = cmplx(res1_real[0], res1_imag[0]); amp[indices[3]] = cmplx(res1_real[1], res1_imag[1]);
