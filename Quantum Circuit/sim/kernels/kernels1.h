062ce44d21a352f8f8c7cc607458f91493c8f346
Aneeqa Fatima <aneeqaf@umich.edu>
2018-01-02 18:18:35 +0300

    Prof. Markov's scheme for 4X implemented - 5.73s for 25q

--- Quantum Circuit/sim/kernels/kernels1.h
+++ Quantum Circuit/sim/kernels/kernels1.h
@@ -13,32 +13,44 @@
 using namespace std;
 
 __attribute__((always_inline)) inline void
-Apply4X12GateHelper(__m256& res_real_add,
-                    __m256& res_imag_add,
-                    __m256& res_real_sub,
-                    __m256& res_imag_sub,
+Apply4X12GateHelper(__m128& res_real_add,
+                    __m128& res1_real_add,
+                    __m128& res_real_sub,
+                    __m128& res1_real_sub,
+                    __m128& res_imag_add,
+                    __m128& res1_imag_add,
+                    __m128& res_imag_sub,
+                    __m128& res1_imag_sub,
                     const cmplx* __restrict a)
 {
-    __m256 t_real = _mm256_setr_ps(real(a[0]), real(a[1]), real(a[4]), real(a[5]),
-                                   real(a[8]), real(a[9]), real(a[12]), real(a[13]));
-    __m256 t1_real = _mm256_setr_ps(real(a[3]), real(a[2]), real(a[7]), real(a[6]),
-                                    real(a[11]), real(a[10]), real(a[15]), real(a[14]));
-    __m256 t_imag = _mm256_setr_ps(imag(a[0]), imag(a[1]), imag(a[4]), imag(a[5]),
-                                   imag(a[8]), imag(a[9]), imag(a[12]), imag(a[13]));
-    __m256 t1_imag = _mm256_setr_ps(imag(a[3]), imag(a[2]), imag(a[7]), imag(a[6]),
-                                    imag(a[11]), imag(a[10]), imag(a[15]), imag(a[14]));
+    __m128 t_real = _mm_setr_ps(real(a[0]), real(a[4]), real(a[8]), real(a[12]));
+    __m128 t1_real = _mm_setr_ps(real(a[1]), real(a[5]), real(a[9]), real(a[13]));
+    __m128 t2_real = _mm_setr_ps(real(a[2]), real(a[6]), real(a[10]), real(a[14]));
+    __m128 t3_real = _mm_setr_ps(real(a[3]), real(a[7]), real(a[11]), real(a[15]));
     
-    __m256 tres_real_add = _mm256_add_ps(t_real, t1_real);
-    __m256 tres_imag_sub = _mm256_sub_ps(t_real, t1_real);
-    __m256 tres_imag_add = _mm256_add_ps(t_imag, t1_imag);
-    __m256 tres_real_sub = _mm256_sub_ps(t1_imag, t_imag);
-    tres_real_sub = _mm256_permute_ps(tres_real_sub, 0b10110001);
-    tres_imag_sub = _mm256_permute_ps(tres_imag_sub, 0b10110001);
+    __m128 t_imag = _mm_setr_ps(imag(a[0]), imag(a[4]), imag(a[8]), imag(a[12]));
+    __m128 t1_imag = _mm_setr_ps(imag(a[1]), imag(a[5]), imag(a[9]), imag(a[13]));
+    __m128 t2_imag = _mm_setr_ps(imag(a[2]), imag(a[6]), imag(a[10]), imag(a[14]));
+    __m128 t3_imag = _mm_setr_ps(imag(a[3]), imag(a[7]), imag(a[11]), imag(a[15]));
     
-    res_real_add = _mm256_add_ps(tres_real_add, tres_real_sub);
-    res_real_sub = _mm256_sub_ps(tres_real_add, tres_real_sub);
-    res_imag_add = _mm256_add_ps(tres_imag_add, tres_imag_sub);
-    res_imag_sub = _mm256_sub_ps(tres_imag_add, tres_imag_sub);
+    __m128 tres_real = _mm_add_ps(t_real, t3_real);
+    __m128 tres1_real = _mm_add_ps(t1_real, t2_real);
+    __m128 tres2_real = _mm_sub_ps(t3_imag, t_imag);
+    __m128 tres3_real = _mm_sub_ps(t2_imag, t1_imag);
+    
+    __m128 tres_imag = _mm_add_ps(t_imag, t3_imag);
+    __m128 tres1_imag = _mm_add_ps(t1_imag, t2_imag);
+    __m128 tres2_imag = _mm_sub_ps(t_real, t3_real);
+    __m128 tres3_imag = _mm_sub_ps(t1_real, t2_real);
+    
+    res_real_add = _mm_add_ps(tres1_real, tres2_real);
+    res1_real_add = _mm_add_ps(tres_real, tres3_real);
+    res_real_sub = _mm_sub_ps(tres_real, tres3_real);
+    res1_real_sub = _mm_sub_ps(tres1_real, tres2_real);
+    res_imag_add = _mm_add_ps(tres1_imag, tres2_imag);
+    res1_imag_add = _mm_add_ps(tres_imag, tres3_imag);
+    res_imag_sub = _mm_sub_ps(tres_imag, tres3_imag);
+    res1_imag_sub = _mm_sub_ps(tres1_imag, tres2_imag);
 }
 
 __attribute__((always_inline)) inline void
@@ -55,49 +67,60 @@ Apply4X12Gate(cmplx* __restrict amp,
 //    ApplyXX12Gate( amp, {indices[8], indices[9], indices[10], indices[11]});
 //    ApplyXX12Gate( amp, {indices[12], indices[13], indices[14], indices[15]});
     
-    __m256 res_real_add, res_imag_add, res_real_sub, res_imag_sub;
+    __m128 res_real_add, res1_real_add, res_real_sub, res1_real_sub,
+    res_imag_add, res1_imag_add, res_imag_sub, res1_imag_sub;
 
     const cmplx a[16] = {amp[indices[0]], amp[indices[4]],  amp[indices[8]], amp[indices[12]],
                         amp[indices[1]], amp[indices[5]],  amp[indices[9]], amp[indices[13]],
                         amp[indices[2]], amp[indices[6]],  amp[indices[10]], amp[indices[14]],
                         amp[indices[3]], amp[indices[7]], amp[indices[11]],  amp[indices[15]]};
 
-    Apply4X12GateHelper(res_real_add, res_imag_add, res_real_sub, res_imag_sub, a);
+    Apply4X12GateHelper(res_real_add, res1_real_add, res_real_sub, res1_real_sub,
+                        res_imag_add, res1_imag_add, res_imag_sub, res1_imag_sub, a);
 
     float* res_real = (float*)&res_real_add;
     float* res_imag = (float*)&res_imag_add;
-    float* res1_real = (float*)&res_real_sub;
-    float* res1_imag = (float*)&res_imag_sub;
+    float* res1_real = (float*)&res1_real_add;
+    float* res1_imag = (float*)&res1_imag_add;
+    float* res2_real = (float*)&res_real_sub;
+    float* res2_imag = (float*)&res_imag_sub;
+    float* res3_real = (float*)&res1_real_sub;
+    float* res3_imag = (float*)&res1_imag_sub;
 
-    amp[indices[0]] = cmplx(res_real[1], res_imag[1]); amp[indices[4]] = cmplx(res_real[0], res_imag[0]);
-    amp[indices[8]] = cmplx(res1_real[0], res1_imag[0]); amp[indices[12]] = cmplx(res1_real[1], res1_imag[1]);
-    amp[indices[1]] = cmplx(res_real[3], res_imag[3]); amp[indices[5]] = cmplx(res_real[2], res_imag[2]);
-    amp[indices[9]] = cmplx(res1_real[2], res1_imag[2]); amp[indices[13]] = cmplx(res1_real[3], res1_imag[3]);
-    amp[indices[2]] = cmplx(res_real[5], res_imag[5]); amp[indices[6]] = cmplx(res_real[4], res_imag[4]);
-    amp[indices[10]] = cmplx(res1_real[4], res1_imag[4]); amp[indices[14]] = cmplx(res1_real[5], res1_imag[5]);
-    amp[indices[3]] = cmplx(res_real[7], res_imag[7]); amp[indices[7]] = cmplx(res_real[6], res_imag[6]);
-    amp[indices[11]] = cmplx(res1_real[6], res1_imag[6]); amp[indices[15]] = cmplx(res1_real[7], res1_imag[7]);
+    amp[indices[0]] = cmplx(res_real[0], res_imag[0]); amp[indices[4]] = cmplx(res1_real[0], res1_imag[0]);
+    amp[indices[8]] = cmplx(res2_real[0], res2_imag[0]); amp[indices[12]] = cmplx(res3_real[0], res3_imag[0]);
+    amp[indices[1]] = cmplx(res_real[1], res_imag[1]); amp[indices[5]] = cmplx(res1_real[1], res1_imag[1]);
+    amp[indices[9]] = cmplx(res2_real[1], res2_imag[1]); amp[indices[13]] = cmplx(res3_real[1], res3_imag[1]);
+    amp[indices[2]] = cmplx(res_real[2], res_imag[2]); amp[indices[6]] = cmplx(res1_real[2], res1_imag[2]);
+    amp[indices[10]] = cmplx(res2_real[2], res2_imag[2]); amp[indices[14]] = cmplx(res3_real[2], res3_imag[2]);
+    amp[indices[3]] = cmplx(res_real[3], res_imag[3]); amp[indices[7]] = cmplx(res1_real[3], res1_imag[3]);
+    amp[indices[11]] = cmplx(res2_real[3], res2_imag[3]); amp[indices[15]] = cmplx(res3_real[3], res3_imag[3]);
     
     const cmplx b[16] = {amp[indices[0]], amp[indices[1]],  amp[indices[2]], amp[indices[3]],
                         amp[indices[4]], amp[indices[5]], amp[indices[6]], amp[indices[7]],
                         amp[indices[8]], amp[indices[9]], amp[indices[10]], amp[indices[11]],
                         amp[indices[12]], amp[indices[13]], amp[indices[14]], amp[indices[15]]};
 
-     Apply4X12GateHelper(res_real_add, res_imag_add, res_real_sub, res_imag_sub, b);
+    Apply4X12GateHelper(res_real_add, res1_real_add, res_real_sub, res1_real_sub,
+                        res_imag_add, res1_imag_add, res_imag_sub, res1_imag_sub, b);
 
     res_real = (float*)&res_real_add;
     res_imag = (float*)&res_imag_add;
-    res1_real = (float*)&res_real_sub;
-    res1_imag = (float*)&res_imag_sub;
-
-    amp[indices[0]] = cmplx(res_real[1], res_imag[1]); amp[indices[1]] = cmplx(res_real[0], res_imag[0]);
-    amp[indices[2]] = cmplx(res1_real[0], res1_imag[0]); amp[indices[3]] = cmplx(res1_real[1], res1_imag[1]);
-    amp[indices[4]] = cmplx(res_real[3], res_imag[3]); amp[indices[5]] = cmplx(res_real[2], res_imag[2]);
-    amp[indices[6]] = cmplx(res1_real[2], res1_imag[2]); amp[indices[7]] = cmplx(res1_real[3], res1_imag[3]);
-    amp[indices[8]] = cmplx(res_real[5], res_imag[5]); amp[indices[9]] = cmplx(res_real[4], res_imag[4]);
-    amp[indices[10]] = cmplx(res1_real[4], res1_imag[4]); amp[indices[11]] = cmplx(res1_real[5], res1_imag[5]);
-    amp[indices[12]] = cmplx(res_real[7], res_imag[7]); amp[indices[13]] = cmplx(res_real[6], res_imag[6]);
-    amp[indices[14]] = cmplx(res1_real[6], res1_imag[6]); amp[indices[15]] = cmplx(res1_real[7], res1_imag[7]);
+    res1_real = (float*)&res1_real_add;
+    res1_imag = (float*)&res1_imag_add;
+    res2_real = (float*)&res_real_sub;
+    res2_imag = (float*)&res_imag_sub;
+    res3_real = (float*)&res1_real_sub;
+    res3_imag = (float*)&res1_imag_sub;
+    
+    amp[indices[0]] = cmplx(res_real[0], res_imag[0]); amp[indices[1]] = cmplx(res1_real[0], res1_imag[0]);
+    amp[indices[2]] = cmplx(res2_real[0], res2_imag[0]); amp[indices[3]] = cmplx(res3_real[0], res3_imag[0]);
+    amp[indices[4]] = cmplx(res_real[1], res_imag[1]); amp[indices[5]] = cmplx(res1_real[1], res1_imag[1]);
+    amp[indices[6]] = cmplx(res2_real[1], res2_imag[1]); amp[indices[7]] = cmplx(res3_real[1], res3_imag[1]);
+    amp[indices[8]] = cmplx(res_real[2], res_imag[2]); amp[indices[9]] = cmplx(res1_real[2], res1_imag[2]);
+    amp[indices[10]] = cmplx(res2_real[2], res2_imag[2]); amp[indices[11]] = cmplx(res3_real[2], res3_imag[2]);
+    amp[indices[12]] = cmplx(res_real[3], res_imag[3]); amp[indices[13]] = cmplx(res1_real[3], res1_imag[3]);
+    amp[indices[14]] = cmplx(res2_real[3], res2_imag[3]); amp[indices[15]] = cmplx(res3_real[3], res3_imag[3]);
 }
 
 __attribute__((always_inline)) inline void
