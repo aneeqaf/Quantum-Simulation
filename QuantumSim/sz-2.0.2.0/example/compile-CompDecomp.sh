28287add2c80ff7f8ed4870b0662dcf92797fda9
Aneeqa Fatima <aneeqaf@umich.edu>
2019-02-03 13:22:55 -0800

    Deleted

--- QuantumSim/sz-2.0.2.0/example/compile-CompDecomp.sh
+++
@@ -1,9 +0,0 @@
-SZPATH=/usr/local
-ZCPATH=/usr/local
-SZFLAG="-I$SZPATH/include -I$ZCPATH/include $SZPATH/lib/libsz.a $SZPATH/lib/libzlib.a $ZCPATH/lib/libzc.a"
-#echo rm testfloat_CompDecomp 
-if [ -f testfloat_CompDecomp ]; then
-	rm testfloat_CompDecomp
-fi
-echo gcc -lm -g -o testfloat_CompDecomp testfloat_CompDecomp.c ${SZFLAG}
-gcc -lm -g -o testfloat_CompDecomp testfloat_CompDecomp.c $SZFLAG
